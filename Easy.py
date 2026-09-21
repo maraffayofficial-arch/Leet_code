e34936849ddf12246e505ba924d2886f89322195
maraffayofficial-arch <m.a.raffay.official@gmail.com>
2026-09-22 04:47:54 +0500

    Problem 2 add2Numbers

--- Easy.py
+++ Easy.py
@@ -68,4 +68,55 @@
 
 
 
+# 2. Add Two Numbers
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+# Example 2:
+
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+# # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# solution 
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy=ListNode(0)
+        curr=dummy
+        carry=0
+    
+
+        while l1 or l2 or carry:
+            # extracting current values 
+            v1=l1.val if l1 else 0
+            v2=l2.val if l2 else 0
+            # now adding the values and calculating the carry value if it exist 
+            total=v1+v2+carry
+            carry=total//10
+            # make a new node 
+            curr.next=ListNode(total%10)
+            curr=curr.next
+
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
+            
+        return dummy.next
         
