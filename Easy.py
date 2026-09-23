f7a15d67a20d658e856c8d2a87a0de0d886bf219
maraffayofficial-arch <m.a.raffay.official@gmail.com>
2026-09-24 02:49:47 +0500

    Problem 5 the Longest Palindromic substring

--- Easy.py
+++ Easy.py
@@ -150,4 +150,140 @@ class Solution(object):
             char_set.add(s[right])
             max_len=max(max_len,right-left+1)
         return max_len
+
+
+
+
+# 4. Median of Two Sorted Arrays
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+# Example 1:
+
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        p1=0
+        p2=0
+        sorted_array=[]
+        while p1<len(nums1) and p2<len(nums2):
+        # for p1 in range(len(nums1)):
+            if nums1[p1]<=nums2[p2]:
+                sorted_array.append(nums1[p1])
+               
+                p1+=1
+            else:
+                sorted_array.append(nums2[p2])
+                
+                p2+=1
+        sorted_array.extend(nums1[p1:])
+        sorted_array.extend(nums2[p2:])
+        
+        median_index=len(sorted_array)/2
+        # print("median_index",median_index)
+
+        if len(sorted_array)%2==0:
+            median_index=median_index-1
+            if sorted_array[int(median_index)]+ sorted_array[int(median_index)+1]==0:
+
+                median_value=0
+            else:
+                median_value=((sorted_array[int(median_index)]+sorted_array[int(median_index)+1])/2)
+            # print("if")
+        else:
+            median_index=int(median_index)
+            median_value=sorted_array[median_index]
+            # print("else")
+            # print("median_index in else",median_index)
             
+        
+        return median_value
+        # print(sorted_array)
+        # print(median_value)
+                
+# Test 
+
+# nums1 = [1,3]
+# nums2 = [2]
+
+# nums1 = [1,2]
+# nums2 = [3,4]
+# nums1 = [0,0]
+# nums2 = [0,0]
+
+nums1 = [2,2,4,4]
+nums2 = [2,2,4,4]
+
+obj=Solution()
+obj.findMedianSortedArrays(nums1,nums2)
+
+
+
+
+
+
+
+# 5. Longest Palindromic Substring
+
+# Given a string s, return the longest palindromic substring in s. 
+
+# Example 1:
+
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+       if len(s)==0:
+            return ""
+       start=0 #palindrome substring ka starting element
+       len_best_pal=1 #longest palindrome ki len
+
+       def longestpalindrome(left,right):
+            while left>=0 and right<len(s) and s[left]==s[right]: # asl main left right say left jayega and right jo hai wo left say right jayega increase hotay huay 
+                # ye tb tk hoga jb tk s[right]==s[left] jesay he ye condition fail hui mtlb k yahan tk he substring paidrome thi or loop break hojayga or
+                # return krdega left+1 kyu k left or right 1 dusray ko cross kr chukay hongay to left 1 extra step aagay hoga to left+1 hamari palindrome ka first element ya start index hoga jo k hman return krna hia 
+                # or isi trhan kun k dono cross hogaye hain to right-left-1 say hmain puray palindrome substring ki length pta chl jayge
+                left-=1
+                right+=1
+                
+            return left+1,right-left-1
+
+# is loop main hum sb substrngs ko compare krengay k sab say lambi konsi hai or start or len_best_pal ko update krtay jaingay
+       for i in range(len(s)):
+            
+            s1,len1=longestpalindrome(i,i)
+            if len1>len_best_pal:
+                start,len_best_pal=s1,len1
+
+            s2,len2=longestpalindrome(i,i+1)
+            if len2>len_best_pal:
+                start,len_best_pal=s2,len2
+
+       return s[start:start+len_best_pal] # slicing start sa shuru and start+len and last element include nhi hoga slicing asay he kam krti hai
+           
+             
+# Test check 
+S=Solution()
+s = "babad"
+
+S.longestPalindrome(s)
+
+
+
+
+
+
+
