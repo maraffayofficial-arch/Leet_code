4c5bf4c14bd3ab1c11d542d5a03f7dd9a7328b74
maraffayofficial-arch <m.a.raffay.official@gmail.com>
2026-09-26 19:54:07 +0500

    Problem 7: Reverse integer

--- Easy.py
+++ Easy.py
@@ -322,3 +322,49 @@ S=Solution()
 S.convert("raffay",3)
 
 
+
+# 7. Reverse Integer
+
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+# Example 1:
+
+# Input: x = 123
+# Output: 321
+
+# this is the simple reversing integer  function
+
+# def reverse(x: int) -> int:
+#     result=0
+#     while x!=0:
+#         digit=x%10
+#         result=result*10+digit
+#         x=x//10
+#     return result
+
+# But the leetcode problem 7 will be done the below way 
+class Solution:
+    
+    def reverse(self, x: int) -> int:
+        int_min,int_max=-2**31,2**31-1
+        sign=-1 if x<0 else 1
+        x_abs=abs(x)
+        res=0
+        while x_abs!=0:
+            digit=x_abs%10
+            x_abs=x_abs//10
+            if res>(int_max-digit)//10:
+                 print("executed")
+                 return 0
+            res=res*10+digit
+        return sign*res
+        
+
+x=123213
+
+ss=Solution()
+ss.reverse(x)
